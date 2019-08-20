c20c9eab71d5b585b759615d23b37d0f5514cf95
Your Name <you@example.com>
2019-08-20 10:44:26 +0700

    Class with extends method

--- index.js
+++ index.js
@@ -95,19 +95,72 @@
 
 // ===========================/Function Parameter====================================
 
-// ES6
-const shoppingList = ['Milk', 'Cow', 'Eggs', 'Bananas', 'Choco']
+// // ES6
+// const shoppingList = ['Milk', 'Cow', 'Eggs', 'Bananas', 'Choco']
 
-//Foreach
-shoppingList.forEach((element, index) => {
-    console.log(`Index of ${index} is ${element} so nice`);
+// //Foreach
+// shoppingList.forEach((element, index) => {
+//     console.log(`Index of ${index} is ${element} so nice`);
     
-});
+// });
 
-//Map 
-const newList = shoppingList.map(item => item + "new") //untuk menambahkan kata new di setiap akhir kata index array
-console.log(newList);
+// //Map 
+// const newList = shoppingList.map(item => item + "new") //untuk menambahkan kata new di setiap akhir kata index array
+// console.log(newList);
 
-//Filter
-const filterList = shoppingList.filter(item => item === "Eggs") //filtering
-console.log(filterList);
+// //Filter
+// const filterList = shoppingList.filter(item => item === "Eggs") //filtering
+// console.log(filterList);
+
+//============================Pengolahan Array====================================
+
+//OLD JS
+    function Person(name, age, hairColor){
+        this.name = name
+        this.age  = age
+        this.hairColor = hairColor
+    }
+
+    Person.prototype.sayName = function(){
+        console.log(`My name is ${this.name}`)
+    };
+
+    function Farhan(ocupation, hobbys, name, age, hairColor){
+        Person.call(this, name, age, hairColor)
+        this.ocupation = ocupation
+        this.hobbys = hobbys
+    }
+
+    Farhan.prototype = Object.create(Person.prototype)
+
+    const person = new Farhan('Dev', 'Motorcycle', 'Farhan', 19, 'Black')
+
+    //var Farhan = new Person('Farhan', 19, 'Black')
+    person.sayName()
+
+//ES 6
+
+class ShoppingList {
+    constructor(items, nr){
+        this.items = items
+        this.nr = nr
+    }
+
+    sayList(){
+        console.log(this.items);
+    }
+}
+
+class Product extends ShoppingList {
+    constructor(items, nr, ammount, cost){
+        super(items, nr)
+        this.ammount = ammount
+        this.cost = cost
+    }
+}
+
+const product = new Product(['Redbull, chocolate, candy'], 3, 2, 20) //panggil extends
+product.sayList() //panggil extends
+
+// const myList = new ShoppingList(['Milk, choco, redbull'], 3) //panggil 1 class
+// myList.sayList() //panggil 1 class
