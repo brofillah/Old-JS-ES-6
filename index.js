acac2511b2c191373bda2f665c87c690ecc5a33e
Your Name <you@example.com>
2019-08-20 08:21:52 +0700

    object

---
+++ index.js
@@ -0,0 +1,20 @@
+// OLD JAVASCRIPT
+
+var user = {
+    name: "Farhan",
+    age: 25
+}
+
+myName = user.name
+console.log(myName);
+
+// ES6
+
+const list = {
+    name: 'Shopping List',
+    items: ['Milk', 'Cow']
+}
+
+const {name, items} = list
+
+console.log(name, items);
